c61d399515d6471d1a4cc697d1d84f4e74064168
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-09-13 00:24:30 +0200

    up

--- sources/main.c
+++ sources/main.c
@@ -31,18 +31,18 @@ int check_adjacent_cell(int grid[4][4], int gap, int num)
 
 int puzzle_solver(int grid[4][4], int pattern[16], int gap)
 {
-	int size;
+	int num;
 
 	/* if solver reaches end return that the grid is resolved */
 	if (gap == 16)
 		return (1);
-	size = 0;
-	while (++size <= 4)
+	num = 0;
+	while (++num <= 4)
 	{
-		if (check_adjacent_cell(grid, gap, size) == 0)
+		if (check_adjacent_cell(grid, gap, num) == 0)
 		{
 			/* put new number in the grid */
-			grid[gap / 4][gap % 4] = size;
+			grid[gap / 4][gap % 4] = num;
 			/* check if new number is correct */
 			if (check_cell(grid, gap, pattern) == 0)
 			{
@@ -75,10 +75,13 @@ int main(int argc, char *argv[])
 	/* if incorrect arguments end the program */
 	if (check_arguments(argc, argv) == 1)
 	{
+		ft_putstr("Error\n");
 		return (0);
 	}
 	/* convert the string pattern as an array of `int` */
 	pattern = convert_pattern(argv[1]);
+	if (!pattern)
+		return (1);
 	/* call main program function, if a solution is found display it */
 	if (puzzle_solver(grid, pattern, 0) == 1)
 	{
